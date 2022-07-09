3acb3acd533f083690c268919d05d56425ed4106
code@jasom.eu <site@arxa.eu>
2022-07-09 13:11:31 +0200

    GenomicRangeQuery

--- GenomicRangeQuery.cs
+++ GenomicRangeQuery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Codility
 {
@@ -9,71 +10,136 @@ namespace Codility
     {
         public GenomicRangeQuery()
         {
-            //var bigS = new string('G', 100000);
-            //var sw = new Stopwatch();
-            //sw.Start();
-            //var aa = solution(bigS, new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 });
-            //sw.Stop();
-            //var ela = sw.ElapsedMilliseconds;
-            var bb = solution("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 });
+            var N_MAX = 100_000;
+            var M_MAX = 50_000;
+
+            //var bb0 = solution("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 });
+            //return;
+            getRandomData(N_MAX, M_MAX, out var s, out var p, out var q);
+            var sw = new Stopwatch();
+            sw.Start();
+            var bb = solution(s, p, q);
+            sw.Stop();
+            var ela = sw.ElapsedMilliseconds;
         }
 
-        Dictionary<Tuple<int, int>, int> memo = new Dictionary<Tuple<int, int>, int>();
-        List<List<int>> ranges = new List<List<int>>();
-        List<int> iS = new List<int>();
+
 
         public int[] solution(string S, int[] P, int[] Q)
         {
-            ranges.Add(new List<int>());
-            ranges.Add(new List<int>());
-            ranges.Add(new List<int>());
-            ranges.Add(new List<int>());
-            ranges.Add(new List<int>());
             List<int> results = new List<int>();
 
+            //search preparations
+            int lastIdxFactor1 = -2, lastIdxFactor2 = -2, lastIdxFactor3 = -2;
+
+            // decode factors
+            var lstFactors = new List<int>();
             for (int i = 0; i < S.Length; i++)
             {
-                var factor = getCharFactor(S[i]);
-                this.iS.Add(factor);
-                //ranges[factor].Add(i);
+                int f;
+                var c = S[i];
+                if (c == 'A')
+                    f = 1;
+                else if (c == 'C')
+                    f = 2;
+                else if (c == 'G')
+                    f = 3;
+                else
+                    f = 4;
+
+                lstFactors.Add(f);
+            }
+            var iS = lstFactors.ToArray();
+
+            /// Item1 - closest right index for factor 1
+            /// Item2 - closest right index for factor 2
+            /// Item3 - closest right index for factor 3
+            var iS2 = new List<Tuple<int, int, int>>();
+
+            // enumerate factors
+            for (int i = 0; i < iS.Length; i++)
+            {
+                // find nearest indexes with optimizations
+                if (lastIdxFactor1 < i)
+                {
+                    lastIdxFactor1 = Array.FindIndex(iS, i, (x) => x == 1);
+                    if (lastIdxFactor1 < 0)
+                        lastIdxFactor1 = iS.Length;
+                }
+                if (lastIdxFactor2 < i)
+                {
+                    lastIdxFactor2 = Array.FindIndex(iS, i, (x) => x == 2);
+                    if (lastIdxFactor2 < 0)
+                        lastIdxFactor2 = iS.Length;
+                }
+                if (lastIdxFactor3 < i)
+                {
+                    lastIdxFactor3 = Array.FindIndex(iS, i, (x) => x == 3);
+                    if (lastIdxFactor3 < 0)
+                        lastIdxFactor3 = iS.Length;
+
+                }
+
+                iS2.Add(new Tuple<int, int, int>(lastIdxFactor1, lastIdxFactor2, lastIdxFactor3));
             }
 
-            for (int iRange = 0; iRange < P.Length; iRange++)
-                results.Add(getFactor(P[iRange], Q[iRange]));
+            // enumerate requests
+            for (int idxRequest = 0; idxRequest < P.Length; idxRequest++)
+            {
+                var start = P[idxRequest];
+                var end = Q[idxRequest];
+                var data = iS2[start];
+                if (data.Item1 <= end)
+                    results.Add(1);
+                else if (data.Item2 <= end)
+                    results.Add(2);
+                else if (data.Item3 <= end)
+                    results.Add(3);
+                else
+                    results.Add(4);
+
+            }
 
             return results.ToArray();
         }
 
-        private int getFactor(int start, int end)
+        private void getRandomData(int N, int M, out string S, out int[] P, out int[] Q)
         {
-            var tpl = new Tuple<int, int>(start, end);
-            if (memo.ContainsKey(tpl))
-                return memo[tpl];
-
-            //  Nucleotides of types A, C, G and T have impact factors of 1, 2, 3 and 4, respectively.
-            if (start == end)
+            /*
+             N 1..100000 - dlzka S
+             M 1..50000 - pocet requestov
+             */
+            var rnd = new Random(7);
+            var sb = new StringBuilder();
+            var lstP = new List<int>();
+            var lstQ = new List<int>();
+            for (int i = 0; i < N; i++)
             {
-                return iS[start];
+                var iC = rnd.Next(4);
+                char c;
+                if (iC == 0) c = 'A'; else if (iC == 1) c = 'C'; else if (iC == 2) c = 'G'; else c = 'T';
+                sb.Append(c);
             }
 
-            var f = Math.Min(iS[end], getFactor(start, end - 1));
-            memo.Add(tpl, f);
-            return f;
+            for (int i = 0; i < M; i++)
+            {
+                var start = rnd.Next(100, N) - 100;
+                var end = start + rnd.Next(50);
+                //var end = rnd.Next(N);
+                if (start > end)
+                {
+                    var x = start;
+                    start = end;
+                    end = x;
+                }
+                lstP.Add(start);
+                lstQ.Add(end);
+            }
+            S = sb.ToString();
+            P = lstP.ToArray();
+            Q = lstQ.ToArray();
         }
 
 
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        int getCharFactor(char c)
-        {
-            if (c == 'A')
-                return 1;
-            else if (c == 'C')
-                return 2;
-            else if (c == 'G')
-                return 3;
-            return 4;
-
-        }
     }
 }
