c2444fd72c3304293850532ab4efc8292268d272
code@jasom.eu <site@arxa.eu>
2022-07-12 14:47:34 +0200

    NumberOfDiscIntersections 50%

---
+++ NumberOfDiscIntersections.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility
+{
+    internal class NumberOfDiscIntersections
+    {
+        public NumberOfDiscIntersections()
+        {
+            solution(new int[] { 1, 5, 2, 1, 4, 0 });
+        }
+
+        public int solution(int[] A)
+        {
+            var data = new Dictionary<int, HashSet<int>>();
+            var hsResults = new HashSet<Tuple<int, int>>();
+
+            for (int posX = 0; posX < A.Length; posX++)
+            {
+                var radius = A[posX];
+                for (int i = posX - radius; i <= posX + radius; i++)
+                {
+                    if (!data.ContainsKey(i))
+                    {
+                        var hs = new HashSet<int>();
+                        hs.Add(posX);
+                        data.Add(i, hs);
+
+                    }
+                    else
+                    {
+                        var di = data[i];
+                        foreach (var prevX in di)
+                        {
+                            var tpl = Tuple.Create(posX, prevX);
+                            hsResults.Add(tpl);
+                        }
+                        di.Add(posX);
+                    }
+                }
+            }
+            return hsResults.Count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Codility
     {
         static void Main(string[] args)
         {
-            new Triangle();
+            new NumberOfDiscIntersections();
+            //new Triangle();
             //new MaxProductOfThree();
             //new Distinct();
             //new MinAvgTwoSlice();
