397fe6303a4060aa27f94bc36198e709216f0c33
code@jasom.eu <site@arxa.eu>
2022-07-13 14:38:29 +0200

    NumberOfDiscIntersections

--- NumberOfDiscIntersections.cs
+++ NumberOfDiscIntersections.cs
@@ -11,31 +11,22 @@ namespace Codility
         public NumberOfDiscIntersections()
         {
             solution(new int[] { 1, 2147483647, 0 }); //; 2
-            //solution(new int[] { 1, 5, 2, 1, 4, 0 }); //; 11
-            //solution(new int[] { 1, 1, 1 }); //; 3
+            solution(new int[] { 1, 5, 2, 1, 4, 0 }); //; 11
+            solution(new int[] { 1, 1, 1 }); //; 3
         }
 
         public int solution(int[] A)
         {
-            var lstRanges = new List<Tuple<long, long>>();
-
-            for (long posX = 0; posX < A.Length; posX++)
-            {
-                long radius = A[posX];
-                var range = Tuple.Create(posX - radius, posX + radius);
-                lstRanges.Add(range);
-            }
-            int count = 0;
-            var ranges = lstRanges.OrderBy(i => i.Item1).ThenBy(i => i.Item2).ToArray();
-
             var lstData = new List<Tuple<long, bool>>();
-            foreach (var item in lstRanges)
+            for (long i = 0; i < A.Length; i++)
             {
-                lstData.Add(Tuple.Create(item.Item1, true));
-                lstData.Add(Tuple.Create(item.Item2, false));
+                int radius = A[i];
+                lstData.Add(Tuple.Create(i - radius, true));
+                lstData.Add(Tuple.Create(i + radius, false));
             }
             var data = lstData.OrderBy(i => i.Item1).ThenByDescending(i => i.Item2).ToArray();
 
+            long count = 0;
             var level = -1;
             foreach (var item in data)
             {
@@ -46,12 +37,12 @@ namespace Codility
                 }
                 else
                     level--;
-                Debug.WriteLine($"{level}, {item.Item1}, {item.Item2}");
+                if (count > 10000000)
+                    return -1;
 
             }
-            Debug.WriteLine($"count: {count}");
 
-            return count;
+            return (int)count;
         }
     }
 
