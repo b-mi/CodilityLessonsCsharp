b6aaaf5058d108d88d4b12c8c5713a1d2a0140d7
code@jasom.eu <site@arxa.eu>
2022-07-16 18:13:56 +0200

    MaxSliceSum

--- MaxSliceSum.cs
+++ MaxSliceSum.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Codility
@@ -8,9 +10,10 @@ namespace Codility
         public MaxSliceSum()
         {
 
-            solution(new int[] { 3, -2, 3 }); // 4
-
-            solution(new int[] { 3, 2, -6, 4, 0 });
+            Debug.Assert(solution(new int[] { 3, -2, 3 }) == 4); // 4
+            Debug.Assert(solution(new int[] { 3, 2, -6, 4, 0 }) == 5); // 5
+            Debug.Assert(solution(new int[] { -7, -2, 3, 2, -6, 4, 3, 2, -1, 0, 7, -4, -5, 8, -13, 1, -5, -3, 8 }) == 15);
+            Debug.Assert(solution(new int[] { -7, -2, 3, 2, -6, 4, 3, 2, -1, 0, 7, -4, -5, 8, -13 }) == 15);
         }
 
         public int solution(int[] A)
@@ -23,16 +26,42 @@ namespace Codility
                 return A.Max();
 
             // has positives and negatives,  –2,147,483,648 and 2,147,483,647 .
-            int maxSum = A.Sum();
+            int maxValue = A.Max();
+            int maxSum = Math.Max(A.Sum(), maxValue);
 
-            var idx = 0;
-            var grps = A.GroupBy(i => i < 0 ? idx++ : idx);
-
-            foreach (var g in grps)
+            // reduction
+            var newA = new List<int>();
+            int idx = 0;
+            while (A[idx] <= 0) idx++; // skip negatives
+            bool isNegative = false;
+            int sum = 0;
+            for (int i = idx; i < A.Length; i++)
             {
-                var max = g.Where(i => i > 0).Sum();
-                if (max > maxSum)
-                    maxSum = max;
+                var aValue = A[i];
+                if (aValue == 0) continue;
+                if (isNegative && aValue < 0 || !isNegative && aValue > 0)
+                    sum += aValue;
+                else
+                {
+                    newA.Add(sum);
+                    sum = aValue;
+                    isNegative = !isNegative;
+                }
+            }
+            if (sum > 0)
+                newA.Add(sum);
+
+            maxSum = Math.Max(newA.Max(), maxSum);
+
+            // finding max
+            for (int i = 0; i < newA.Count - 0; i += 2)
+            {
+                sum = newA[i];
+                for (int j = i + 1; j < newA.Count - 0; j += 2)
+                {
+                    sum += newA[j] + newA[j + 1];
+                    maxSum = Math.Max(sum, maxSum);
+                }
             }
 
             return maxSum;
