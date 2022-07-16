4cb14de713b1946cd52de7df3a84adcbbeb8eaf6
code@jasom.eu <site@arxa.eu>
2022-07-16 14:23:46 +0200

    MaxProfit

--- MaxProfit.cs
+++ MaxProfit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -10,11 +11,10 @@ namespace Codility
         {
 
             Debug.Assert(solution(new int[] { 8, 9, 3, 6, 1, 2 }) == 3);
-
-            //Debug.Assert(solution(new int[] { 5, 4, 3, 2, 1 }) == 0);
-            //Debug.Assert(solution(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 }) == 356);
-            //Debug.Assert(solution(new int[] { }) == 0);
-            //Debug.Assert(solution(new int[] { 3, 3 }) == 0);
+            Debug.Assert(solution(new int[] { 5, 4, 3, 2, 1 }) == 0);
+            Debug.Assert(solution(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 }) == 356);
+            Debug.Assert(solution(new int[] { }) == 0);
+            Debug.Assert(solution(new int[] { 3, 3 }) == 0);
 
 
 
@@ -23,19 +23,39 @@ namespace Codility
         {
             if (A.Length == 0)
                 return 0;
-            int idx = 0;
-            var a = A.Select(i => new { idx = idx++, data = i }).OrderBy(i => i.data).ToArray();
-            while (true)
+
+            var lst = new List<int[]>();
+            var bestProfit = int.MinValue;
+
+            int startValue = A[0];
+            var maxValue = startValue;
+
+            for (int i = 1; i < A.Length; i++)
             {
-
+                var currentValue = A[i];
+                if (currentValue >= maxValue)
+                    maxValue = currentValue;
+                else
+                {
+                    updateMax(lst, maxValue, startValue);
+                    startValue = maxValue = currentValue;
+                }
             }
-            var minValue = a.First();
-            var maxValue = a.LastOrDefault(i => i.idx > minValue.idx);
-            if (maxValue == null)
-                return 0;
-            int bestProfit = maxValue.data - minValue.data;
+            updateMax(lst, maxValue, startValue);
 
-            return Math.Max(0, bestProfit);
+            bestProfit = lst.Max(i => i[1] - i[0]);
+            return bestProfit;
+        }
+
+        private static void updateMax(List<int[]> lst, int maxValue, int startValue)
+        {
+            int j = lst.Count - 1;
+            while (j >= 0 && lst[j][1] < maxValue && lst[j][0] < maxValue)
+            {
+                lst[j][1] = maxValue;
+                j--;
+            }
+            lst.Add(new int[] { startValue, maxValue });
         }
     }
 }
