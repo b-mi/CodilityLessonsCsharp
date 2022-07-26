92030c68f50ba63a25a690789df8604f683444ec
code@jasom.eu <site@arxa.eu>
2022-07-26 22:15:21 +0200

    MaxDoubleSliceSum

--- MaxDoubleSliceSumTools.cs
+++ MaxDoubleSliceSumTools.cs
@@ -9,6 +9,202 @@ namespace Codility
     {
         Random rnd;
 
+
+        public int solution_old1(int[] A)
+        {
+            var c = A.Skip(1).Take(A.Length - 2).ToArray();
+            var hasPositives = c.Any(i => i > 0);
+            var hasNegatives = c.Any(i => i < 0);
+            if (!hasNegatives)
+            {
+                var bn = brute(A, out int xn, out int yn, out int zn);
+                var rtn = c.Sum() - c.Min();
+                Debug.WriteLine($"Brute: max: {bn}, max sol: {rtn}, arr: {string.Join(",", A)}");
+
+                return rtn;
+            }
+            if (!hasPositives)
+            {
+                Debug.WriteLine($"negatives: 0, arr: {string.Join(",", A)}");
+                return 0;
+            }
+
+            #region reduction, result in b
+            //var lstR = new List<int>();
+
+            //lstR.Add(0);
+            //int idx = 0;
+            //int sumR = 0;
+            //bool hasPos = false;
+            //while (idx < c.Length)
+            //{
+            //    var value = c[idx++];
+            //    if (value < 0)
+            //    {
+            //        if (hasPos) // add positives sum
+            //        {
+            //            lstR.Add(sumR);
+            //            sumR = 0;
+            //            hasPos = false;
+            //        }
+
+            //        lstR.Add(value);
+            //    }
+            //    else
+            //    {
+            //        sumR += value;
+            //        hasPos = true;
+            //    }
+
+            //}
+            //if (hasPos)
+            //    lstR.Add(sumR);
+            //lstR.Add(0);
+
+            //if (c.Sum() != lstR.Sum())
+            //{
+            //    throw new NotImplementedException();
+            //}
+
+            //var b = lstR.ToArray();
+            #endregion
+
+            var b = A;
+
+            var bruteRtn = brute(b, out int xR, out int yR, out int zR);
+
+            //var bruteRtn = 48;
+            //int xR = 6, yR = 13, zR = 19;
+
+
+            //var bruteRtn = 14;
+            //int xR = 119, yR = 126, zR = 184;
+
+            int sum = 0, sum2 = 0, x = 0, y = 1, z = 2, maxPoint = 0, minPoint = 0, summin = 0, summin2 = 0;
+
+            sum2 = A.Sum() - A[0] - A[1] - A[A.Length - 1];
+            int maxSum = int.MinValue;
+            int minSum = int.MaxValue;
+
+
+            while (y < b.Length - 2)
+            {
+
+                sum += b[y];
+                y++;
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxPoint = y;
+                    Debug.WriteLine($"U: y: {y}, sum: {sum}, sum2 {sum2},");
+                }
+                if (summin < minSum)
+                {
+                    minSum = summin;
+                    minPoint = y;
+                    Debug.WriteLine($"Umin: y: {y}, summin: {summin}, summin2 {summin2},");
+                }
+
+                sum2 -= b[y];
+                if (sum2 > maxSum)
+                {
+                    maxSum = sum2;
+                    maxPoint = y;
+                    Debug.WriteLine($"D: y: {y}, sum: {sum}, sum2 {sum2},");
+                }
+                if (summin2 < minSum)
+                {
+                    minSum = summin2;
+                    minPoint = y;
+                    Debug.WriteLine($"Dmin: y: {y}, summmin: {summin}, summmin2 {summin2},");
+                }
+            }
+
+            //
+            //hladanie od bestPoint v oboch smeroch
+            // 
+            sum = 0;
+            var maxR = int.MinValue;
+            for (int idx = maxPoint + 1; idx < A.Length - 1; idx++)
+            {
+                sum += A[idx];
+                if (sum > maxR)
+                    maxR = sum;
+            }
+
+            sum = 0;
+            int maxL = int.MinValue;
+            for (int idx = maxPoint - 1; idx > 0; idx--)
+            {
+                sum += A[idx];
+                if (sum > maxL)
+                    maxL = sum;
+            }
+
+            maxL = Math.Max(0, maxL);
+            maxR = Math.Max(0, maxR);
+            maxSum = maxL + maxR;
+
+            if (maxSum == bruteRtn)
+            {
+                Debug.WriteLine("OK");
+            }
+            else
+            {
+                Debug.WriteLine($"BAD {bruteRtn} != {maxSum}, bestPoint: {maxPoint}, BR: ({xR}-{yR}-{zR})");
+                Debug.WriteLine($"{string.Join(",", A)}");
+
+            }
+
+            //// y => last z
+            //while (z < b.Length - 1)
+            //{
+            //    sum += b[z];
+            //    z++;
+            //    if (sum > maxSum)
+            //    {
+            //        maxSum = Math.Max(maxSum, sum);
+            //        bestX = x; bestY = y; bestZ = z;
+            //    }
+            //}
+            //Debug.WriteLine($"PH1: x: {bestX}, y: {bestY}, z: {bestZ}, sum: {maxSum}");
+
+
+            //// y => z - 1
+            //while (y < z - 1)
+            //{
+            //    sum += b[y] - b[y + 1];
+            //    y++;
+            //    if (sum > maxSum)
+            //    {
+            //        maxSum = Math.Max(maxSum, sum);
+            //        bestX = x; bestY = y; bestZ = z;
+            //    }
+            //}
+            //Debug.WriteLine($"PH2: x: {bestX}, y: {bestY}, z: {bestZ}, sum: {maxSum}");
+
+            //// x => y - 1
+            //while (x < y - 1)
+            //{
+            //    x++;
+            //    sum -= b[x];
+            //    if (sum > maxSum)
+            //    {
+            //        maxSum = Math.Max(maxSum, sum);
+            //        bestX = x; bestY = y; bestZ = z;
+            //    }
+            //}
+            //Debug.WriteLine($"PH3: x: {bestX}, y: {bestY}, z: {bestZ}, sum: {maxSum}");
+            //Debug.WriteLine($"Brute: x: {xR}, y: {yR}, z: {zR}, max: {bruteRtn}, arr: {string.Join(",", A)}");
+            //Debug.WriteLine($"BR: ({xR}-{yR}-{zR}), brMax: {bruteRtn}");
+            //Debug.WriteLine(string.Join(",", A));
+            Debug.WriteLine("---");
+
+
+            return maxSum;
+        }
+
+
         //private int[] reduceLst(int[] lst)
         //{
         //    var lst2 = new List<int>();
